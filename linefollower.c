e793b2357afe7b37fbcc31cb12b5daafe5af2660
mustafab0 <mustafabhadsorawala@gmail.com>
2022-04-16 11:03:13 -0400

    added look function and added linefollower to readline.cpp

--- linefollower.c
+++ linefollower.c
@@ -15,19 +15,22 @@ unsigned int stack2[40+25];
 void printFun(void* par1);
 int main()                                    // Main function
 {
-  simpleterm_close();
-  // Add startup code here.
+    // Add startup code here.
   pause(250);
+ 
+  simpleterm_close();
+  //int cogn1= cogstart(printFun,NULL,stack2,sizeof(stack2));
+  int cogno= cogstart(lineFollower,NULL,stack1,sizeof(stack1));
+  
+  
+  while(1){
+  pause(5000);
   simpleterm_open();
-  print("In cog0");
+  pause(250);
+  print("In cog0\n");
   simpleterm_close();
   
-  
-  //simpleterm_close();
-  //int cogn1= cogstart(printFun,NULL,stack2,sizeof(stack2));
-  int cogno= cogstart(readPosition,NULL,stack1,sizeof(stack1));
-  
-  
+  }  
 /*
    while(1){
    simpleterm_open();
@@ -46,69 +49,6 @@ void printFun(void* par){
   print("In cog0");
   simpleterm_close();
 }  
-}  
-
-
-#include "simpletools.h"                      // Include simple tools
-#include "servo.h"                            // Include servo library
-
-void lineFollower();
-int constrain(int amt, int low, int high);
-void look(char k);
-
-int GOAL = 3000;
-const double KP = 0.035; //0.04                
-const double KD = 0.03;
-volatile int position;
-volatile double lastError = 0;
-
-// Global vars for cogs to share
-unsigned int stack1[40 + 25]; // Stack vars for cog1
-
-
-int main()                                    // Main function
-{
-  position=3000;
-  pause(1000); // 1 s delay
-/*  
-  for(int n = 0; n <= 50; n+=1){
-
-  lineFollower();
- 
-}  
-*/
-cogstart(lineFollower, NULL, stack1, sizeof(stack1));
-}
-
-
-void lineFollower(){
-
-  //printf(OUTA1=%n\n",position);
-  int error = GOAL - position;		
-  //We check difference between goal and current position
-  //printf("error= ");
-  //printf(error);
- 
-  // Compute motor adjustment
-  int adjustment = KP*error + KD*(error - lastError); //PID tuning
- 
-  // Store error for next increment
-  lastError = error;
-  //printf("adjustment= ");
-  //printf(adjustment);
-  //printf('\t');
-  //delay(250);
-  
-  
-  // servoLeft.writeMicroseconds(constrain(1550 + adjustment, 1490, 1550)); 
-  servo_speed(17,constrain(100 + adjustment, 75, 100));        
-  // servoRight.writeMicroseconds(constrain(1420 + adjustment, 1420, 1490));        // We make slight adjustments based on the KP and KD we calculated
-  servo_speed(16,constrain(100 + adjustment, 75, 100));        
-  pause(1);
-}
-
-int constrain(int amt, int low, int high) {
-  ((amt)<(low)?(low):((amt)>(high)?(high):(amt)));
 }  
 
 
@@ -132,7 +72,7 @@ void look(char k){
       default:
       break;
     }
- }
+}
 
 
 /*
